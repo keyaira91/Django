fe16525b4302cdbb0b10ac2f53aa9d0919105cc5
keyaira91 <keyaira22@hotmail.com>
2021-09-21 17:13:28 -0400

    built additional pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,5 +4,12 @@ from . import views
 app_name = 'learning_logs'
 
 urlpatterns = [
+    # Home Page
     path('', views.index, name='index'),
+
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]

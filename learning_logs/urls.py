1661645856bcfbb1372cd3d2b7198ae2aad29767
keyaira91 <keyaira22@hotmail.com>
2021-09-22 22:45:22 -0400

    adding new topic and edit entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,12 +4,14 @@ from . import views
 app_name = 'learning_logs'
 
 urlpatterns = [
-    # Home Page
-    path('', views.index, name='index'),
-
     # Page that shows all topics
     path('topics/', views.topics, name='topics'),
 
     # Detail page for a single topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Home Page
+    path('', views.index, name='index'),
 ]
